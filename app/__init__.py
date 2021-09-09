270d96bfa1b7765ba125440332b70de4ddde0483
TERESIA012 <kingoriteresia@gmail.com>
2021-09-09 10:28:35 +0300

    watchlist

---
+++ app/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+from .main import main as main_blueprint
+
+bootstrap = Bootstrap()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init_app(app)
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # registering the blueprint
+    
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .request import configure_request
+    configure_request(app)
+
+    return app
+
+
+
+
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='88ca8d610e7b0c920904712569df287f'
+export SECRET_KEY='os.urandom(32)'
+
+python3 manage.py server
