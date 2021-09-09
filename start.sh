65c79d787b0d1250e459fae0bb0c0e0ea193be0c
TERESIA012 <kingoriteresia@gmail.com>
2021-09-09 11:27:53 +0300

    modify heroku

--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
-    {
-        export MOVIE_API_KEY='88ca8d610e7b0c920904712569df287f'
+export MOVIE_API_KEY='88ca8d610e7b0c920904712569df287f'
 export SECRET_KEY='os.urandom(32)'
-    }
 
 python3.8 manage.py server
