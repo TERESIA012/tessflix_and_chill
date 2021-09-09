5d83caa275e54f97af0b64a9accc7f0d77f5b638
TERESIA012 <kingoriteresia@gmail.com>
2021-09-09 10:58:10 +0300

    deploy to heroku

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
-export MOVIE_API_KEY='88ca8d610e7b0c920904712569df287f'
+    {
+        export MOVIE_API_KEY='88ca8d610e7b0c920904712569df287f'
 export SECRET_KEY='os.urandom(32)'
+    }
 
 python3 manage.py server
